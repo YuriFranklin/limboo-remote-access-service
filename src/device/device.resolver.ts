3441dbfd873d46c612095c5a3642c8455a85e7f6
Yuri Franklin <yurifranklin98@gmail.com>
2024-10-11 11:53:17 -0500

    :construction: wip: federation

--- src/device/device.resolver.ts
+++ src/device/device.resolver.ts
@@ -24,7 +24,6 @@ import { GetAllDeviceOutput } from './dto/get-all-device.output';
 import { LogService } from 'src/log/log.service';
 import { ConfigService } from '@nestjs/config';
 import { EventType, LogLevel } from 'src/log/log.entity';
-import { User } from 'src/user/user.entity';
 
 @Resolver(() => Device)
 @UseGuards(AuthGuard, RoleGuard)
@@ -36,12 +35,12 @@ export class DeviceResolver {
     private readonly configService: ConfigService,
   ) {}
 
-  @ResolveField(() => User)
+  @ResolveField('owner')
   async owner(@Parent() device: Device) {
     return { __typename: 'User', id: device.ownerId };
   }
 
-  @ResolveField(() => [User])
+  @ResolveField('coOwners')
   async coOwners(@Parent() device: Device) {
     return device.coOwnersId?.map((id) => ({ __typename: 'User', id }));
   }
