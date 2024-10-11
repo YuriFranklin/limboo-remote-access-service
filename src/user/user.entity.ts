d232e4af30a533fb89e8d0a9580e339d3ce67f9c
Yuri Franklin <yurifranklin98@gmail.com>
2024-10-11 12:24:19 -0500

    :construction: wip: federation

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,9 +1,16 @@
 import { Directive, Field, ID, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
+@Directive('@extends')
 @Directive('@key(fields: "id")')
 export class User {
   @Field(() => ID)
   @Directive('@external')
   id: string;
+
+  @Field(() => [User], { nullable: true })
+  coOwners?: User[];
+
+  @Field(() => User, { nullable: true })
+  owner?: User;
 }
