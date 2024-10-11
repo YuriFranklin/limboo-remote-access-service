2b7925b0a26e5c36c5163f8cf3d99d044815ee76
Yuri Franklin <yurifranklin98@gmail.com>
2024-10-11 11:21:52 -0500

    :construction: wip: federation

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -9,26 +9,20 @@ export class User {
   id: string;
 
   @Field()
-  @Directive('@external')
   username: string;
 
   @Field()
-  @Directive('@external')
   enabled: boolean;
 
   @Field()
-  @Directive('@external')
   firstName: string;
 
   @Field({ nullable: true })
-  @Directive('@external')
   lastName?: string;
 
   @Field()
-  @Directive('@external')
   createdAt: string;
 
   @Field({ nullable: true })
-  @Directive('@external')
   email?: string;
 }
